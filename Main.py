445e337b03a746f0ef24f855389ed1311cd60206
Simon jigalin <simon.jigalin@gmail.com>
2023-05-22 17:33:19 +0200

    tsp

--- Main.py
+++ Main.py
@@ -1,15 +1,64 @@
-import random
+
+# population_size muss minimum tournament size sein
 import numpy as np
 import matplotlib.pyplot as plt
-
-# EA Parameter
-population_size = 100
-mutation_rate = 0.1
-crossover_rate = 0.5
-num_generations = 100
+import random
 
 
-# Klasse zur Repräsentation einer Route
+# population_size muss minimum tournament size sein
+population_size = 10
+mutation_rate = 1
+refraction_rate = 1
+num_generations = 5
+selection_size = 10
+
+def generate_Berlin52_List():
+    # input file --> Open
+    infile = open('berlin52.tsp', 'r')
+
+    # Aufteilen der Infozeilen von Berlin52
+    name = infile.readline().strip().split()[1]
+    filetype = infile.readline().strip().split()[1]
+    comment = infile.readline().strip().split()[1]
+    dimension = infile.readline().strip().split()[1]
+    edgeweighttype = infile.readline().strip().split()[1]
+    infile.readline()
+
+    # Erstellen der Liste mit den Koordinaten Tupeln, HIER Tupel = float
+    cities = []
+    N = int(dimension)
+    for i in range(0, N):
+        x, y = infile.readline().strip().split()[1:]
+        cities.append((float(x), float(y)))
+
+    # input file --> Close
+    infile.close()
+    return cities
+
+
+def generate_ST70_List():
+    # input file --> Open
+    infile = open('st70.tsp', 'r')
+
+    # Aufteilen der Infozeilen von st70
+    name = infile.readline().strip().split()[1]
+    filetype = infile.readline().strip().split()[1]
+    comment = infile.readline().strip().split()[1]
+    dimension = infile.readline().strip().split()[1]
+    edgeweighttype = infile.readline().strip().split()[1]
+    infile.readline()
+
+    # Erstellen der Liste mit den Koordinaten Tupeln, HIER Tupel = int
+    cities = []
+    N = int(dimension)
+    for i in range(0, N):
+        x, y = infile.readline().strip().split()[1:]
+        cities.append((int(x), int(y)))
+
+    # input file --> Close
+    infile.close()
+    return cities
+
 class Route:
     def __init__(self, cities):
         self.cities = cities
@@ -22,38 +71,56 @@ class Route:
         for i in range(num_cities - 1):
             city1 = self.cities[i]
             city2 = self.cities[i + 1]
+            # Euklidischer Abstand
             total_distance += np.linalg.norm(np.array(city1) - np.array(city2))
+
+        city1 = self.cities[0]
+        city2 = self.cities[num_cities - 1]
+        total_distance += np.linalg.norm(np.array(city2) - np.array(city1))
         return total_distance
 
 
-# Erzeugung einer zufälligen Route
+# Erzeugen einer zufälligen Route bei einem zufälligem Startpunkt
 def generate_random_route(cities):
     route = Route(cities)
-    random.shuffle(route.cities)
-    return route
+    # Stätte in der Liste werden neu angeordnet
+    # random.shuffle(route.cities)
+    new_cities = random.sample(route.cities, len(cities))
+    return Route(new_cities)
 
 
-# Erzeugung einer zufälligen Population von Routen
 def generate_random_population(cities, population_size):
     population = []
     for _ in range(population_size):
         route = generate_random_route(cities)
+        # append fügt einer Liste ein element hinzu, KEINE KONTROLLE
         population.append(route)
     return population
 
 
-# Selektion von Eltern für die Rekombination
-def selection(population, tournament_size):
+def selection(population, selection_size):
     selected_parents = []
-    for _ in range(2):
-        tournament = random.sample(population, tournament_size)
-        winner = min(tournament, key=lambda route: route.distance)
-        selected_parents.append(winner)
+    tournament = random.sample(population, selection_size)
+    index = None
+    best_route = None
+    j = 0
+    while j < 2:
+        for i in range(len(tournament) - 1):
+            if best_route is None:
+                index = 0
+                best_route = tournament[0]
+            elif tournament[i].distance < best_route.distance:
+                index = i
+                best_route = tournament[i]
+        selected_parents.append(best_route)
+        del tournament[index]
+        index = None
+        best_route = None
+        j += 1
     return selected_parents
 
 
-# Rekombination zweier Routen
-def crossover(parent1, parent2):
+def refraction(parent1, parent2):
     cities = parent1.cities.copy()
     child1 = cities.copy()
     child2 = cities.copy()
@@ -63,7 +130,6 @@ def crossover(parent1, parent2):
     child2[start_index:end_index] = parent2.cities[start_index:end_index]
     index = end_index % len(cities)
     max_attempts = len(cities)
-
     for _ in range(max_attempts):
         if None not in child1 and None not in child2:
             break
@@ -82,10 +148,6 @@ def crossover(parent1, parent2):
     return Route(child1), Route(child2)
 
 
-
-
-
-# Mutation einer Route durch Vertauschen zweier Städte
 def mutation(route, mutation_rate):
     cities = route.cities
     if random.random() < mutation_rate and len(cities) >= 2:
@@ -94,9 +156,23 @@ def mutation(route, mutation_rate):
         route.distance = route.calculate_distance()
 
 
+def test_Route(route, original):
+    cities = route.cities
+    counter = len(original)
+    test_list = []
+    for i in cities:
+        if i in original and i not in test_list:
+            counter -= 1
+            test_list.append(i)
+
+    if counter == 0:
+        return True
+    else:
+        return False
+
 
 # Evolutionärer Algorithmus für das TSP
-def tsp_evolutionary_algorithm(cities, population_size, num_generations, tournament_size, mutation_rate):
+def tsp_evolutionary_algorithm(cities, population_size, num_generations, selection_size, mutation_rate):
     population = generate_random_population(cities, population_size)
     best_distances = []
     best_route = None
@@ -105,8 +181,8 @@ def tsp_evolutionary_algorithm(cities, population_size, num_generations, tournam
         new_population = []
 
         while len(new_population) < population_size:
-            parents = selection(population, tournament_size)
-            child1, child2 = crossover(parents[0], parents[1])
+            parents = selection(population, selection_size)
+            child1, child2 = refraction(parents[0], parents[1])
             mutation(child1, mutation_rate)
             mutation(child2, mutation_rate)
             new_population.extend([child1, child2])
@@ -114,52 +190,35 @@ def tsp_evolutionary_algorithm(cities, population_size, num_generations, tournam
         population = new_population
         best_route = min(population, key=lambda route: route.distance)
         best_distances.append(best_route.distance)
-        print("Generation:", generation + 1, "Beste Strecke:", best_route.distance)
+        print(best_route.cities)
+        if test_Route(best_route, cities):
+            print("Generation:", generation + 1, "Beste Strecke:", best_route.distance)
+        else:
+            print("Keine gültige Route wurde gebildet")
 
     return best_route, best_distances
 
 
-# Testinstanzen aus TSPLIB
-# Beispielinstanz: berlin52
-berlin52 = [
-    (565, 575), (25, 185), (345, 750), (945, 685), (845, 655), (880, 660),
-    (25, 230), (525, 1000), (580, 1175), (650, 1130), (1605, 620), (1220, 580),
-    (1465, 200), (1530, 5), (845, 680), (725, 370), (145, 665), (415, 635),
-    (510, 875), (560, 365), (300, 465), (520, 585), (480, 415), (835, 625),
-    (975, 580), (1215, 245), (1320, 315), (1250, 400), (660, 180), (410, 250),
-    (420, 555), (575, 665), (1150, 1160), (700, 580), (685, 595), (685, 610),
-    (770, 610), (795, 645), (720, 635), (760, 650), (475, 960), (95, 260),
-    (875, 920), (700, 500), (555, 815), (830, 485), (1170, 65), (830, 610),
-    (605, 625), (595, 360), (1340, 725), (1740, 245)
-]
+berlin52 = generate_Berlin52_List()
 
-# Beispielinstanz: st70
-st70 = [
-    (64, 96), (80, 39), (69, 23), (72, 42), (48, 67), (58, 43), (81, 60), (79, 10), (30, 39), (37, 72),
-    (29, 10), (7, 23), (2, 64), (64, 96), (80, 39), (69, 23), (72, 42), (48, 67), (58, 43), (81, 60),
-    (79, 10), (30, 39), (37, 72), (29, 10), (7, 23), (2, 64), (64, 96), (80, 39), (69, 23), (72, 42),
-    (48, 67), (58, 43), (81, 60), (79, 10), (30, 39), (37, 72), (29, 10), (7, 23), (2, 64), (64, 96),
-    (80, 39), (69, 23), (72, 42), (48, 67), (58, 43), (81, 60), (79, 10), (30, 39), (37, 72), (29, 10),
-    (7, 23), (2, 64), (64, 96), (80, 39), (69, 23), (72, 42), (48, 67), (58, 43), (81, 60), (79, 10),
-    (30, 39), (37, 72), (29, 10), (7, 23), (2, 64)
-]
+st70 = generate_ST70_List()
 
-# Evolutionärer Algorithmus für Testinstanz berlin52
-best_route_berlin52, best_distances_berlin52 = tsp_evolutionary_algorithm(berlin52, population_size, num_generations, 5,
-                                                                          mutation_rate)
+# Evolutionärer Algorithmus für Testinstanz berlin52 best_route_berlin52, best_distances_berlin52 =
+# tsp_evolutionary_algorithm(berlin52, population_size, num_generations, selection_size, mutation_rate)
 
 # Evolutionärer Algorithmus für Testinstanz st70
-best_route_st70, best_distances_st70 = tsp_evolutionary_algorithm(st70, population_size, num_generations, 5,
+best_route_st70, best_distances_st70 = tsp_evolutionary_algorithm(st70, population_size, num_generations,
+                                                                  selection_size,
                                                                   mutation_rate)
 
 # Ausgabe der Ergebnisse
-print("Beste Strecke für berlin52:", best_route_berlin52.distance)
-print("Beste Strecke für st70:", best_route_st70.distance)
+# print("Beste Strecke für berlin52:", best_route_berlin52.distance)
+# print("Beste Strecke für st70:", best_route_st70.distance)
 
 # Darstellung der Konvergenz der besten Strecke
-plt.plot(best_distances_berlin52, label="berlin52")
-plt.plot(best_distances_st70, label="st70")
-plt.xlabel("Generation")
-plt.ylabel("Beste Strecke")
-plt.legend()
-plt.show()
+# plt.plot(best_distances_berlin52, label="berlin52")
+# plt.plot(best_distances_st70, label="st70")
+# plt.xlabel("Generation")
+# plt.ylabel("Beste Strecke")
+# plt.legend()
+# plt.show()
